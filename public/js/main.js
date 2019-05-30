6236870c8bf9415ff3fba7ddc68f09120004b258
mathurin petit <mathurin.petit@gmail.com>
2019-05-30 20:38:59 +0200

    unbeaudiscours

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,19 @@
   var allMusiques = $("#ytplayerMusiques").data('all');
   var allDiscours = $("#ytplayerDiscours").data('all');
 
+  var currentMusicId = null;
+  var currentDiscoursId = null;
+
   function onPlayerReadyMusiques(event) {
-        startTimeCodeVolume(event,$("#ytplayerMusiques").data('id'),'musique');
+        currentMusicId = $("#ytplayerMusiques").data('id');
+        startTimeCodeVolume(event,currentMusicId,'musique');
+        refreshTitre();
   }
 
   function onPlayerReadyDiscours(event) {
-        startTimeCodeVolume(event,$("#ytplayerDiscours").data('id'),'discours');
+        currentDiscoursId = $("#ytplayerDiscours").data('id');
+        startTimeCodeVolume(event,currentDiscoursId,'discours');
+        refreshTitre();
       }
 
   function startTimeCodeVolume(event,id,type) {
@@ -31,12 +38,50 @@
 
   function onPlayerStateChangeMusiques(event) {
         if (event.data === 0) {
-          alert("musique stop");
+          var newObj = randomProperty(allMusiques);
+          var newId = newObj[0];
+          var newList = $("#ytplayerMusiques").attr('data-all');
+          $('#musicContainer').remove($('#ytplayerMusiques'));
+          $('#musicContainer').html('<div id="ytplayerMusiques" data-id="'+newId+'" data-all="'+newList+'" ></div>')
+
+          playerMusiques = new YT.Player('ytplayerMusiques', {
+            height: '360',
+            width: '640',
+            videoId: $('#ytplayerMusiques').data('id'),
+            events: {
+                  'onReady': onPlayerReadyMusiques,
+                  'onStateChange': onPlayerStateChangeMusiques
+                }
+          });
         }
       }
 
   function onPlayerStateChangeDiscours(event) {
         if (event.data === 0) {
-          alert("discours stop");
+          var newObj = randomProperty(allDiscours);
+          var newId = newObj[0];
+          var newList = $("#ytplayerDiscours").attr('data-all');
+          $('#discoursContainer').remove($('#ytplayerDiscours'));
+          $('#discoursContainer').html('<div id="ytplayerDiscours" data-id="'+newId+'" data-all="'+newList+'" ></div>')
+
+          playerMusiques = new YT.Player('ytplayerDiscours', {
+            height: '360',
+            width: '640',
+            videoId: $('#ytplayerDiscours').data('id'),
+            events: {
+                  'onReady': onPlayerReadyDiscours,
+                  'onStateChange': onPlayerStateChangeDiscours
+                }
+          });
         }
       }
+
+  function refreshTitre(){
+    if(typeof allDiscours[currentDiscoursId] !== 'undefined' && typeof allMusiques[currentMusicId] !== 'undefined')
+    $("#titre").html("<small style='color:white;'>"+allDiscours[currentDiscoursId][3]+ "</small><br/>VS<br/><small style='color:white;'>" +allMusiques[currentMusicId][3]+"</small>");
+  }
+
+  var randomProperty = function (obj) {
+    var keys = Object.keys(obj)
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,15 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
+    {
+      $listeMusiques = $this->arrayFromCsvPath('../data/musiques.csv');
+      $listeDiscours = $this->arrayFromCsvPath('../data/discours.csv');
+
+      $discoursIds = array_keys($listeDiscours);
+      $discoursId = $discoursIds[array_rand($discoursIds)];
+
+      $musiqueIds = array_keys($listeMusiques);
+      $musiqueId = $musiqueIds[array_rand($musiqueIds)];
+
+      return $this->redirectToRoute('lecture', array('musiqueId' => $musiqueId,'discoursId' => $discoursId));
+
+    }
+
+    /**
+     * @Route("/lecture/{musiqueId}/{discoursId}")
+     */
+    public function lectureAction(Request $request, $musiqueId , $discoursId)
     {
         $listeMusiques = $this->arrayFromCsvPath('../data/musiques.csv');
         $listeDiscours = $this->arrayFromCsvPath('../data/discours.csv');
@@ -19,9 +40,16 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'listeMusiques' => $listeMusiques,
             'listeDiscours' => $listeDiscours,
-            'musiqueId' => "jhiXkSKWbnU",
-            'discoursId' => "o1sQAvB39N4"
+            'musiqueId' => $musiqueId,
+            'discoursId' => $discoursId
         ]);
+    }
+
+    /**
+     * @Route("/apropos")
+     */
+    public function aProposAction(Request $request) {
+      return $this->render('default/apropos.html.twig', []);
     }
 
     private function arrayFromCsvPath($path){
